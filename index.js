4bcef8b9029eeab1d765d28258871ac3fa1660e8
Song Simon Joon-Hee <g2junhee@cdf.toronto.edu>
2013-02-09 23:45:33 -0500

    Editted Simon's code to list topics by pressing button

--- index.js
+++ index.js
@@ -6,6 +6,17 @@ $(document).ready(function() {
     $.get('/jsonall', function (json) {
         $('#topic1').append(json);
     });
+
+
+	//simon's code for appending new topic, needs to be editted
+	$('#add').click(function() {
+		var topicTitle = document.getElementById("topic");
+		var link = document.getElementById("addlink");
+		$('#topiclist').append('<div id = 1>'+topicTitle.value+'</div>');
+		$('$topiclist').append('(< class="title" href="'+link.value+'">link</a>');
+		$('#topiclist').append('<div id="vote"> vote:0 </div>');
+	});
+
 });
 
     
